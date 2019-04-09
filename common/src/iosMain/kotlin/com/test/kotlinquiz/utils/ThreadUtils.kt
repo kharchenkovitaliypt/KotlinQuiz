53247ccfdbed3e38fa2997b6ed84905f235e63e2
Vitaliy <kharchenkovitaliypt@gmail.com>
2019-04-09 14:25:20 +0300

    Added basic ios project

--- common/src/iosMain/kotlin/com/test/kotlinquiz/utils/ThreadUtils.kt
+++ common/src/iosMain/kotlin/com/test/kotlinquiz/utils/ThreadUtils.kt
@@ -1,3 +1,5 @@
 package com.test.kotlinquiz.utils
 
+import platform.Foundation.NSThread
+
 actual val currentThread: Any get() = NSThread.currentThread
---
+++ ios/KotlinQuizIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KotlinQuizIOS
+//
+//  Created by Vitaliy on 4/9/19.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
