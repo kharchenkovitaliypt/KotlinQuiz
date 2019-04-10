04033406e2f8bb9f51bad286273fd233396a6c9a
Vitaliy <kharchenkovitaliypt@gmail.com>
2019-04-10 16:53:13 +0300

    Implemented IOS version

--- ios/KotlinQuizIOS/ViewController.swift
+++ ios/KotlinQuizIOS/ViewController.swift
@@ -14,8 +14,12 @@ class ViewController: UIViewController {
     //MARK: Properties
     @IBOutlet weak var question: UILabel!
     @IBOutlet weak var totalPoints: UITextField!
+    @IBOutlet weak var input: UITextView!
+    @IBOutlet weak var nextBtn: UIButton!
     
-    var viewModel: QuizViewModel?
+    var selectedAnswer: OptAnswer?
+    
+    var viewModel: QuizViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +27,67 @@ class ViewController: UIViewController {
         let quizService = QuizService(assetService: AssetService())
         self.viewModel = QuizViewModel(quizService: quizService)
         
-        viewModel!.totalPointsLD.observe {
-            let points = $0 as! KotlinLong
-            self.totalPoints.text = "Total points: \(points)"
+        viewModel!.totalPointsLiveData.observe {
+            self.totalPoints.text = "Total points: \($0)"
             return KotlinUnit()
         }
         
-        viewModel!.questionLD.observe {
-            self.question.text = $0?.text
+        viewModel!.questionStateLiveData.observe {
+            let state = $0
+            switch state {
+            case let valueState as QuestionState.Value:
+                self.showQuestion(question: valueState.value)
+            case let doneState as QuestionState.Done:
+                self.showDone(totalPoints: doneState.totalPoints)
+            default: break
+            }
             return KotlinUnit()
         }
     }
     
+    func showQuestion(question: Question) {
+        self.question.text = question.text
+        
+        switch question {
+        case is InputQuestion:
+            input.text = ""
+        case is OptQuestion:
+            let answers = (question as! OptQuestion).answers
+            selectedAnswer = answers.randomElement()!
+            input.text = answers
+                .map { "- " + $0.content + ($0 === selectedAnswer ? "*" : "") }
+                .joined(separator: "\n")
+        default: break
+        }
+    }
+    
+    func showDone(totalPoints: Int64) {
+        self.totalPoints.center = self.view.center
+        self.input.isHidden = true
+        self.question.text = ""
+        self.nextBtn.isHidden = true
+    }
+    
     //MARK: Actions
     @IBAction func onNext(_ sender: UIButton) {
-        viewModel!.processAnswer(answer: "Funny")
+        let question = (viewModel!.questionStateLiveData.data as! QuestionState.Value).value
+        switch question {
+        case is InputQuestion:
+            if let text = input.text, !text.isEmpty {
+                viewModel.processAnswer(answer: text)
+            } else {
+                showAlert(msg: "Please input something")
+            }
+        case is OptQuestion:
+            viewModel.processAnswer(answer_: selectedAnswer!)
+        default: break
+        }
+    }
+    
+    func showAlert(msg: String) {
+        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
